23c5f59598d31331b47d8644e0b27cbaa89b58f2
pandhu <pandhu@afra.id>
2018-06-14 16:07:46 +0700

    home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -49,7 +49,11 @@
           </b-col>
           <b-col cols="12" class="nopadding">
             <div class="sub-price"></div>
-            <b-button @click="goto('form')" class="btn-dftr">
+            
+            <b-button v-if="isRegistered" :href="mainUrl+'profile/'+participant.url" class="btn-dftr">
+              <span class="white">LIHAT PROFIL ANDA</span>
+            </b-button>
+            <b-button v-else @click="goto('form')" class="btn-dftr">
               <span class="white">DAFTAR SEKARANG</span>
             </b-button>
             <b-col cols="12" class="col-text-about">
@@ -170,6 +174,7 @@ export default {
   data () {
     return {
       isRegistered: false,
+      participant: null,
       user: null,
       show: true,
       types: [
@@ -296,7 +301,8 @@ export default {
       axios(url).then(response =>{
         let participant = response.data.result.participant;
         if(participant){
-          this.$set(this, 'isRegistered', true);
+          this.$set(this, 'participant', participant)
+          this.$set(this, 'isRegistered', true)
         }
       }).catch(e => {
 
